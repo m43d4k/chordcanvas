0a1b2b9c004c50e4ecce8dc5069e11c8d9d99afc
m43d4k <m43d4k@gmail.com>
2026-03-24 00:21:06 +0900

    Build to docs directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
